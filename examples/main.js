c5106407732db5da900b70fd1fcd614f35cca736
顾凯铭 <gukaiming@gukaimingdeMBP.lan>
2021-12-19 16:07:44 +0800

    fix:build

--- examples/main.js
+++ examples/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import baseUi from '../pakages/index.js'
+import baseUi from '../packages'
 Vue.config.productionTip = false
 Vue.use(baseUi)
 new Vue({
