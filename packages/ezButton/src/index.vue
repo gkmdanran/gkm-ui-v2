4663f0005d2574bd0524eb24b2053703b4ab22fd
kaiming.gu_sx <kaiming.gu_sx@aispeech.com>
2022-04-29 17:46:07 +0800

    按钮节流

--- packages/ezButton/src/index.vue
+++ packages/ezButton/src/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-button v-bind="$attrs" v-on="$listeners" @click="handleClick">
+  <el-button v-bind="$attrs" @click="handleClick">
     <slot></slot>
   </el-button>
 </template>
@@ -11,39 +11,69 @@ export default {
     confirm: {
       type: Object | String,
     },
+    throttle: {
+      type: Number,
+      default: 0,
+    },
   },
   data() {
-    return {};
+    return {
+      status: {
+        disabled: false,
+      },
+      timstamp: 0,
+    };
   },
+
   methods: {
-    handleClick() {
-      if (!this.confirm) return;
-      if (typeof this.confirm === "string") {
-        this.$confirm(this.confirm, "提示", {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning",
-        })
-          .then(() => {
-            this.$emit("confirmClick");
+    handleClick(event) {
+      //节流
+      if (new Date().valueOf() - this.timstamp < this.throttle) return;
+      let emitObj =
+        this.throttle === -1 ? { event, status: this.status } : event;
+
+      if (this.confirm === undefined) {
+        !this.status.disabled && this.$emit("click", emitObj);
+        this.timstamp = new Date().valueOf();
+        if (this.throttle == -1) {
+          this.status.disabled = true;
+        }
+      } else if (typeof this.confirm === "string") {
+        !this.status.disabled &&
+          this.$confirm(this.confirm, "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning",
           })
-          .catch(() => {
-            this.$emit("cancelClick");
-          });
+            .then(() => {
+              this.$emit("confirmClick", emitObj);
+              this.timstamp = new Date().valueOf();
+              if (this.throttle == -1) {
+                this.status.disabled = true;
+              }
+            })
+            .catch(() => {
+              this.$emit("cancelClick");
+            });
       } else {
         if (!this.confirm.message)
           throw new Error("confirm.message is required");
-        this.$confirm(this.confirm.message, this.confirm.title || "提示", {
-          confirmButtonText: this.confirm.confirmText || "确定",
-          cancelButtonText: this.confirm.cancelText || "取消",
-          type: this.confirm.type || "warning",
-        })
-          .then(() => {
-            this.$emit("confirmClick");
+        !this.status.disabled &&
+          this.$confirm(this.confirm.message, this.confirm.title || "提示", {
+            confirmButtonText: this.confirm.confirmText || "确定",
+            cancelButtonText: this.confirm.cancelText || "取消",
+            type: this.confirm.type || "warning",
           })
-          .catch(() => {
-            this.$emit("cancelClick");
-          });
+            .then(() => {
+              this.$emit("confirmClick", emitObj);
+              this.timstamp = new Date().valueOf();
+              if (this.throttle == -1) {
+                this.status.disabled = true;
+              }
+            })
+            .catch(() => {
+              this.$emit("cancelClick");
+            });
       }
     },
   },
