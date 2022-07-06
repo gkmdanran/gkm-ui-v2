30343100108adaa76e5f4dfdf1d097f86b5a0b79
kaiming.gu_sx <kaiming.gu_sx@aispeech.com>
2022-07-06 16:16:28 +0800

    ezEditInput单元测试

--- tests/unit/ezEditInput.spec.js
+++ tests/unit/ezEditInput.spec.js
@@ -22,7 +22,7 @@ describe('ezEditInput.vue', () => {
         expect(wrapper.emitted('input')[0]).toEqual(['some value'])
     })
     it('测试点击确定后变成read模式并触发change', () => {
-        const wrapper = mount(ezEditInput, {
+        const wrapper = shallowMount(ezEditInput, {
             propsData: {
                 value,
                 mode: "edit"
@@ -35,5 +35,45 @@ describe('ezEditInput.vue', () => {
         wrapper.find('.inputbtn').vm.$emit('click')
         expect(wrapper.vm.selfMode).toBe('read')
         expect(wrapper.emitted('change')[0]).toEqual([value])
+    })
+    it('测试必填校验，及错误展示', async () => {
+        const wrapper = shallowMount(ezEditInput, {
+            propsData: {
+                value: '',
+                mode: "edit",
+                required: true,
+                showError: true
+            },
+            components: {
+                elButton: Button,
+                elInput: Input
+            }
+        })
+        await wrapper.find('.inputbtn').vm.$emit('click')
+        expect(wrapper.find('.err-msg').text()).toBe('内容不能为空')
+        expect(wrapper.emitted('error')[0]).toEqual(['内容不能为空'])
+    })
+    it('测试校验函数', async () => {
+        function verifyValue(value) {
+            return new Promise((resolve, reject) => {
+                let reg = /^[\A-\F0-9]+$/;
+                if (!reg.test(value)) return reject("格式有误");
+                resolve(value);
+            });
+        }
+        const wrapper = mount(ezEditInput, {
+            propsData: {
+                value: '99*',
+                mode: "edit",
+                validator: verifyValue
+            },
+            components: {
+                elButton: Button,
+                elInput: Input
+            }
+        })
+        const validFn = wrapper.vm.validator
+        const cmpValue = wrapper.vm.value
+        await expect(validFn(cmpValue)).rejects.toBe('格式有误');
     })
 })
